c938d089de9f0446b9d011f7299514197f39091b
yvandouenel <y.douenel@coopernet.fr>
2021-06-11 14:08:30 +0200

    Récupération du token, de l'utilisateur et des termes.

--- js/memo/js/App.js
+++ js/memo/js/App.js
@@ -1,83 +1,150 @@
 "use strict";
-import DomHandle from './DomHandle.js';
-import Card from './Card.js';
+import DomHandle from "./DomHandle.js";
+import Card from "./Card.js";
+import FetchData from "./services/FetchData.js";
 
 class App extends DomHandle {
-    constructor() {
-        super();
-        this.updating_card = null;
-        // creation du formulaire d'ajout
-        this.form_add_card = this.drawForm();
+  constructor() {
+    super();
+    this.fd = new FetchData();
+    this.token = "";
+    this.user = null;
+    this.fd.getToken()
+      .then((value) => {
+        console.log("token dans le constructeur : ", value);
+        this.token = value;
+        return this.fd.getUser(this.token, "infres", "12345678");
+      })
+      .then((value) => {
+        console.log("User le constructeur : ", value);
+        this.user = {
+            uid: value.current_user.uid,
+            login: value.name,
+            pwd: "12345678"
+        } 
+        return this.fd.getTerms(this.user, this.token);
+      })
+      .then(data => {
+          console.log(`Termes dans le constructeur d'APP : `, data);
+      })
+      .catch((error) => {
+        console.error("Erreur: ", error.message);
+      });
 
-        // creation du formulaire de mise à jour
-        this.form_up_card = null;
+    this.updating_card = null;
+    // creation du formulaire d'ajout
+    this.form_add_card = this.drawForm();
 
-        // gestion des événements
-        this.form_add_card.onsubmit = (event) => {
-            // Supprime la gestion par défaut des événements
-            event.preventDefault();
-            const question = document.getElementById("question").value;
-            const answer = document.getElementById("answer").value;
+    // creation du formulaire de mise à jour
+    this.form_up_card = null;
 
-            const card1 = new Card(question, answer, this);
+    // gestion des événements
+    this.form_add_card.onsubmit = (event) => {
+      // Supprime la gestion par défaut des événements
+      event.preventDefault();
+      const question = document.getElementById("question").value;
+      const answer = document.getElementById("answer").value;
 
-        }
+      const card1 = new Card(question, answer, this);
+    };
+  }
+  handleUpCard() {
+    this.form_up_card.onsubmit = (event, card_elt) => {
+      console.log(
+        `Dans la gestion de la soumission de l'up de la carte !!!!!!!`
+      );
+      event.preventDefault();
+      const input_question =
+        this.form_up_card.querySelector("#question-up").value;
+      const input_answer = this.form_up_card.querySelector("#answer-up").value;
 
-    }
-    handleUpCard() {
-        this.form_up_card.onsubmit = (event, card_elt) => {
-            console.log(`Dans la gestion de la soumission de l'up de la carte !!!!!!!`);
-            event.preventDefault();
-            const input_question = this.form_up_card.querySelector("#question-up").value;
-            const input_answer = this.form_up_card.querySelector("#answer-up").value;
-            
-            this.updating_card.question = input_question;
-            this.updating_card.answer = input_answer;
-            this.updating_card.dom_elts.question_elt.textContent = input_question;
-            this.updating_card.dom_elts.answer_elt.textContent = input_answer;
+      this.updating_card.question = input_question;
+      this.updating_card.answer = input_answer;
+      this.updating_card.dom_elts.question_elt.textContent = input_question;
+      this.updating_card.dom_elts.answer_elt.textContent = input_answer;
 
-            // Suppression du formulaire de modification
-            this.updating_card = null;
-            this.form_up_card.remove();
-        }
-    }
-    drawForm() {
-        const main_elt = document.getElementById("main");
-        const form_add_card = this.createDomElement("form","",main_elt,{"id": "form-add-card"});
-        
-        const label_question = this.createDomElement("label","Question",form_add_card,{"for": "question"});
-        const input_question = this.createDomElement("input","",form_add_card,{"id":"question", type: "text"});
+      // Suppression du formulaire de modification
+      this.updating_card = null;
+      this.form_up_card.remove();
+    };
+  }
+  drawForm() {
+    const main_elt = document.getElementById("main");
+    const form_add_card = this.createDomElement("form", "", main_elt, {
+      id: "form-add-card",
+    });
 
-        const label_answer = this.createDomElement("label","Réponse",form_add_card,{"for": "answer"});
-        const input_answer = this.createDomElement("input","",form_add_card,{"id":"answer", type: "text"});
+    const label_question = this.createDomElement(
+      "label",
+      "Question",
+      form_add_card,
+      { for: "question" }
+    );
+    const input_question = this.createDomElement("input", "", form_add_card, {
+      id: "question",
+      type: "text",
+    });
 
-        const input_submit = this.createDomElement("input","",form_add_card,{"id":"submit-add", type: "submit", value:"Ajouter"});
+    const label_answer = this.createDomElement(
+      "label",
+      "Réponse",
+      form_add_card,
+      { for: "answer" }
+    );
+    const input_answer = this.createDomElement("input", "", form_add_card, {
+      id: "answer",
+      type: "text",
+    });
 
-        return form_add_card;
-        
-    }
-    drawFormUp(question, answer) {
-        const main_elt = document.getElementById("main");
-        const form_add_card = this.createDomElement(
-            "form",
-            "",
-            main_elt,
-            {"id": "form-up-card", "class": "hidden"});
-        this.createDomElement("h2","Modification de la carte",form_add_card);
-        const label_question = this.createDomElement("label","Question",form_add_card,{"for": "question"});
-        const input_question = this.createDomElement("input","",form_add_card,{"id":"question-up", type: "text", "value": question});
+    const input_submit = this.createDomElement("input", "", form_add_card, {
+      id: "submit-add",
+      type: "submit",
+      value: "Ajouter",
+    });
 
-        const label_answer = this.createDomElement("label","Réponse",form_add_card,{"for": "answer"});
-        const input_answer = this.createDomElement("input","",form_add_card,{"id":"answer-up", type: "text", "value": answer});
+    return form_add_card;
+  }
+  drawFormUp(question, answer) {
+    const main_elt = document.getElementById("main");
+    const form_add_card = this.createDomElement("form", "", main_elt, {
+      id: "form-up-card",
+      class: "hidden",
+    });
+    this.createDomElement("h2", "Modification de la carte", form_add_card);
+    const label_question = this.createDomElement(
+      "label",
+      "Question",
+      form_add_card,
+      { for: "question" }
+    );
+    const input_question = this.createDomElement("input", "", form_add_card, {
+      id: "question-up",
+      type: "text",
+      value: question,
+    });
 
-        const input_submit = this.createDomElement("input","",form_add_card,{"id":"submit-up", type: "submit", value:"Modifier"});
+    const label_answer = this.createDomElement(
+      "label",
+      "Réponse",
+      form_add_card,
+      { for: "answer" }
+    );
+    const input_answer = this.createDomElement("input", "", form_add_card, {
+      id: "answer-up",
+      type: "text",
+      value: answer,
+    });
 
-        return form_add_card;
-        
-    }
+    const input_submit = this.createDomElement("input", "", form_add_card, {
+      id: "submit-up",
+      type: "submit",
+      value: "Modifier",
+    });
+
+    return form_add_card;
+  }
 }
 
 new App();
 /* const card1 = new Card("Qui est l'inventeur du js", "Brendan Eich");
 console.log(`card1 : `, card1); */
-
