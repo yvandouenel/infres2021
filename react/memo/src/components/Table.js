8d35e0a53abe3597ced16eb7bb045d3167281f5c
yvandouenel <y.douenel@coopernet.fr>
2021-06-17 10:47:17 +0200

    Début de mémo en react.

--- react/memo/src/components/Table.js
+++ react/memo/src/components/Table.js
@@ -1,13 +1,24 @@
 import React, { Component } from "react";
-
+import Term from "./Term.js";
 class Table extends Component {
-  state = {};
+  state = {
+    terms: [
+      {id: 1, name: "js"},
+      {id: 2, name: "React"},
+      {id: 3, name: "test"},
+    ]
+  };
   render() {
     return (
       <>
-        <header>Header</header>
-        <main>Main</main>
-        <footer>Footer</footer>
+        <header className="container">
+          <h1 className="text-center">Memo</h1>
+          <nav className="d-flex justify-content-center">
+            {this.state.terms.map(term => <Term name={term.name} />)}
+          </nav>
+        </header>
+        <main className="container">Main</main>
+        <footer className="container">Footer</footer>
       </>
     );
   }
---
+++ react/memo/src/components/Term.js
@@ -0,0 +1,4 @@
+
+export default function Term(props) {
+    return <button className="m-3 btn btn-warning">{`${props.name}`}</button>;
+  }
