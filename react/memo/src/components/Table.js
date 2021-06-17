0f6a6b09bbcf09f201acd9848ec8af2209bea97f
yvandouenel <y.douenel@coopernet.fr>
2021-06-17 12:05:29 +0200

    Récupération du token ok.

--- react/memo/src/components/Table.js
+++ react/memo/src/components/Table.js
@@ -1,25 +1,73 @@
 import React, { Component } from "react";
 import Term from "./Term.js";
+import FetchData from "../services/FetchData.js";
 
 /* Faites en sorte que Table n'affiche pas par défaut les Termes mais plutôt un formulaire d'authentification. Une fois le formulaire renseigné, les termes pourront apparaître */
 class Table extends Component {
-  state = {
-    terms: [
-      {id: 1, name: "js"},
-      {id: 2, name: "React"},
-      {id: 3, name: "test"},
-    ]
-  };
+  constructor(props) {
+    super(props);
+    this.fd = new FetchData();
+    this.state = {
+      terms: [
+        { id: 1, name: "js" },
+        { id: 2, name: "React" },
+        { id: 3, name: "test" },
+      ],
+      token: ""
+    };
+  }
+  componentDidMount() {
+    // Une fois que le component est monté, je fais appel aux web services
+    console.log(`fd dans componentDidMount :`, this.fd);
+    this.fd.getToken()
+    .then(token => {
+      const state = {...this.state};
+      state.token = token;
+      this.setState(state);
+    })
+    .catch((error) => {
+      console.error("Erreur attrapée dans getToken : ", error.message);
+    })
+
+  }
+  handleSubmitFormLogin(event){
+    event.preventDefault();
+    console.log(`Formulaire soumis`);
+    const state = {...this.state};//Copie par valeur du state
+    state.display_form = false;
+    // Si le state local est différent du stage du composant et que cela a un 
+    // impact sur le rendu, alors la méthode render est rappelée
+    this.setState(state);
+  }
   render() {
     return (
       <>
         <header className="container">
           <h1 className="text-center">Memo</h1>
-          <nav className="d-flex justify-content-center">
-            {this.state.terms.map(term => <Term name={term.name} />)}
+
+          {!this.state.token && (
+            <nav className="d-flex justify-content-center">
+            {this.state.terms.map((term) => (
+              <Term key={term.id} name={term.name} />
+            ))}
           </nav>
+          )}
         </header>
-        <main className="container">Main</main>
+        <main className="container">
+        {this.state.token && (
+            <form action="" onSubmit={(event) => {this.handleSubmitFormLogin(event);}}>
+              <label htmlFor="login">Login</label>
+              <input type="text" id="login"/>
+              <label htmlFor="pwd">Mot de passe</label>
+              <input type="password" id="pwd"/>
+              <input type="submit" value="Se connecter" />
+            </form>
+          )}
+          {!this.state.token && (
+            <p className="text-danger">L'application n'a pu récupérer communiquer avec le serveur</p>
+          )}
+        </main>
+
         <footer className="container">Footer</footer>
       </>
     );
--- react/memo/src/components/Term.js
+++ react/memo/src/components/Term.js
@@ -1,4 +1,7 @@
-
+// Stateless component : c'est à dire que le composant n'a pas de state et qu'il n'a
+// donc pas besoin d'hériter de la classe "Component" 
+// En revanche, il récupère des arguments passés par son composant parent (Table) via
+// le paramètre props
 export default function Term(props) {
     return <button className="m-3 btn btn-warning">{`${props.name}`}</button>;
   }
