ef9d9cb6ac10699dc0238150fb15194b074bd5f1
yvandouenel <y.douenel@coopernet.fr>
2021-06-18 08:34:37 +0200

    exo ajout carte.

--- react/memo/src/components/Card.js
+++ react/memo/src/components/Card.js
@@ -1,5 +1,5 @@
 /* 
-Transformer de component stateless en component avec state
+Transformer ce component stateless en component avec state
 Dans le state, vous ajoutez une propriété du type show_answer qui sera 
 à false par défaut
 Dans la méthode render de ce composant, vous n'afficherez la réponse que si
@@ -7,11 +7,25 @@ show_answer est à true
 Il faudra donc gérer l'événement click sur la question pour obtenir 
 un comportement similaire à celui de memopus.com
 */
-export default function Card(props) {
-  return (
-    <article className="bg-secondary text-light p-3 rounded mb-4">
-      <h4>{props.card.question}</h4>
-      <p>{props.card.reponse}</p>
-    </article>
-  );
+import React, { Component } from "react";
+export default class Card extends Component {
+  state = {
+    show_answer: false,
+  };
+  handleClickAnswer = () => {
+    const state = {...this.state}
+    state.show_answer = !state.show_answer;
+    this.setState(state);
+  }
+  render() {
+    return (
+      <article className="bg-secondary text-light p-3 rounded mb-4">
+        <h4
+        role="button"
+        onClick={this.handleClickAnswer}
+        >{this.props.card.question}</h4>
+        {this.state.show_answer && <p>{this.props.card.reponse}</p>}
+      </article>
+    );
+  }
 }
--- react/memo/src/components/Column.js
+++ react/memo/src/components/Column.js
@@ -1,4 +1,7 @@
 import Card from "./Card.js"
+/* Faites en sorte qu'un bouton cliquable soit ajouté
+en haut de chaque colonne. Au click, un formulaire apparaît.
+Il permet d'ajouter une carte (question et réponse) dans la bonne colonne */
 // Stateless component : c'est à dire que le composant n'a pas de state et qu'il n'a
 // donc pas besoin d'hériter de la classe "Component"
 // En revanche, il récupère des arguments passés par son composant parent (Table) via
