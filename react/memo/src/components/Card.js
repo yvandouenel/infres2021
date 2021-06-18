b02f53223ccfa049878e95c48eb428a0df1cba4c
yvandouenel <y.douenel@coopernet.fr>
2021-06-18 10:56:27 +0200

    Exo sur la modification d'une carte.

--- react/memo/src/components/Card.js
+++ react/memo/src/components/Card.js
@@ -1,11 +1,11 @@
 /* 
-Transformer ce component stateless en component avec state
-Dans le state, vous ajoutez une propriété du type show_answer qui sera 
-à false par défaut
-Dans la méthode render de ce composant, vous n'afficherez la réponse que si
-show_answer est à true
-Il faudra donc gérer l'événement click sur la question pour obtenir 
-un comportement similaire à celui de memopus.com
+Ajouter un bouton "modifier". Ce dernier va afficher un formulaire de modification.
+Regardez la documentation des formulaires react pour comprendre comment "alimenter" les 
+champs question et réponse (value).
+A la validation de ce formulaire, la carte sera modifiée
+Vous pouvez soit utiliser l'événement "onChange" sur chaque input, soit au contraire, ignorer 
+l'événement "onChange" pour tout gérer via l'événement "onSubmit" du formulaire. Il faudra alors
+utiliser l'attribut "defaultValue" pour que les champs soient tout de même déjà renseignés.
 */
 import React, { Component } from "react";
 export default class Card extends Component {
